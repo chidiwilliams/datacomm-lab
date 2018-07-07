78dde73812e59952403c1cec0d9bc02208b5b7ab
Williams Chidi <williamschidi1@gmail.com>
2018-07-08 00:19:38 +0100

    Used epsilon for signal test

--- test/signals/Signal.js
+++ test/signals/Signal.js
@@ -2,6 +2,10 @@ const expect = require('chai').expect;
 const math = require('mathjs');
 const lab = require('../../dist');
 
+const mth = math.create({
+  epsilon: 1e-5,
+});
+
 const fRes = () => {
   // Generate sine wave with frequency of 4
   const res = new lab.CarrierSignal(16, 4).getFrequencyResponse();
@@ -11,11 +15,11 @@ const fRes = () => {
     .slice(0, 4)
     .concat(res.slice(5))
     .forEach((x, i, a) => {
-      expect(math.abs(x)).to.be.lessThan(0.008);
+      expect(mth.equal(x, 0)).to.be.true;
     });
 
   // Frequency 4 must have magnitude of 1
-  expect(math.abs(1 - res[4])).to.be.lessThan(0.008);
+  expect(mth.equal(res[4], 1)).to.be.true;
 };
 
 module.exports = {
