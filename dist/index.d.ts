edffe4ffe00e6c135da1eabba6fb5353dcbd3d96
Chidi Williams <williamschidi1@gmail.com>
2020-02-02 18:50:12 +0100

    remove dist directory

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { Signal } from './signals/Signal';
-export { WaveSignal, WaveSignalType } from './signals/WaveSignal';
-export { BPSK } from './modulations/BPSK';
-export { AWGN } from './impairments/AWGN';
-export { Hamming4 } from './codecs/Hamming4';
-export { Convolutional } from './codecs/Convolutional';
-export { Filter, FilterType } from './filters/Filter';
-export { Functions } from './functions/Functions';
